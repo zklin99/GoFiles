192cb1171d828da9663a03175c59d6ac7a18e4fb
jax <jax_lin_work@outlook.com>
2025-03-03 01:10:05 +0800

    123

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"io/fs"
 	"log"
+	"os"
 	"path/filepath"
 
 	"github.com/gin-gonic/gin"
@@ -12,18 +12,50 @@ func root(c *gin.Context) {
 	c.String(200, "Hello World")
 }
 
+type fileInfo struct {
+	Name  string `json:"name"`
+	size  int64
+	IsDir bool `json:"is_dir"`
+}
+
 func getFtpFolder(c *gin.Context) {
-	folder := fs.FS(nil)
+	ftpFolderDir, err := os.ReadDir("./ftp_folder")
+	if err != nil {
+		log.Println(err)
+	}
+	log.Printf(
+		"ftp_folder: %v, 文件数量：%d",
+		ftpFolderDir, len(ftpFolderDir),
+	)
+
+	files := make([]fileInfo, 0, len(ftpFolderDir))
+
+	print(files)
+	for _, file := range ftpFolderDir {
+		_fileInfo := fileInfo{
+			Name:  file.Name(),
+			IsDir: file.IsDir(),
+		}
+		files = append(files, _fileInfo)
+	}
+
+	c.IndentedJSON(200, files)
 	path := filepath.Dir("./ftp_folder")
 
 	c.String(200, path)
 }
 
 func main() {
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("当前目录为：%v", dir)
+
 	r := gin.Default()
 	r.GET("/", root)
 	r.GET("/ftp_folder", getFtpFolder)
-	err := r.Run("0.0.0.0:8080")
+	err = r.Run("0.0.0.0:13939")
 
 	if err != nil {
 		log.Fatal(err)
